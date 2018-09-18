893bd3b161d4aed270561b37f97288f150872036
eitan <eitanlev@gmail.com>
2018-09-18 21:55:24 +0300

    added tests

--- test/Deposit.test.js
+++ test/Deposit.test.js
@@ -4,6 +4,8 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const provider = ganache.provider();
 const web3 = new Web3(provider);
+const INIT_VALUE = 1;
+const FEE_VALUE = 1;
 
 // Importing the contracts
 const compiledFactory = require('../ethereum/build/DepositFactory.json');
@@ -19,6 +21,7 @@ let initiator;
 beforeEach(async () => {
   // console.log('see.. this function is run EACH time');
   accounts = await web3.eth.getAccounts();
+  initialValue = INIT_VALUE;
   initiator = accounts[0];
 
   factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
@@ -28,7 +31,8 @@ beforeEach(async () => {
 
   // Not sure why - but these two lines solve the factory issue. TODO understand
   // and apply these changes
-  factory.options.gasPrice = '20000000000000'; // default gas price in wei
+  //factory.options.gasPrice = '20000000000000'; // default gas price in wei
+  factory.options.gasPrice = '2000'; // default gas price in wei
   factory.options.gas = 5000000; // provide as fallback always 5M gas
 
   // Using the factory's method "createDeposit" to create a new deposit
@@ -36,7 +40,8 @@ beforeEach(async () => {
   await factory.methods.createDeposit().send({
     from: initiator,
     gas: '3000000',
-    value: '1'
+    value: initialValue + FEE_VALUE
+    //value: '1'
   });
 
   //Fancy way to do const array = await...; depositAddress = array[0];
@@ -50,16 +55,31 @@ beforeEach(async () => {
 });
 
 describe('Deposits', () => {
-  it('deploys a factory and a deposit', () => {
-    assert.ok(factory.options.address);
-    assert.ok(deposit.options.address);
-  });
+	it('deploys a factory and a deposit', () => {
+		assert.ok(factory.options.address);
+		assert.ok(deposit.options.address);
+	});
 
-  it('marks caller as the deposit manager', async () => {
-    const manager = await deposit.methods.initiator().call();
-    assert.equal(accounts[0], manager);
-  });
+	it('marks caller as the deposit manager', async () => {
+		const manager = await deposit.methods.initiator().call();
+		assert.equal(initiator, manager);
+	});
 
+	it('validate contract was deployed', async () => {
+		assert.equal(depositAddress, await factory.methods.deployedDeposits(initiator,0).call());
+	});
+
+	it('validate initial values upon creation', async () => {
+		const isKeySet = await deposit.methods.isKeySet().call();
+		assert.equal(false, isKeySet, "Key should be false upon creation");
+		const counterpart = await deposit.methods.counterpart().call();
+		assert.equal(0, counterpart, "Counterpart should not be set yet");
+		const initiatorInitialDeposit = await deposit.methods.viewCurrentDeposit(initiator).call();
+		assert.equal(initiatorInitialDeposit, initialValue, "Initial value should be equal to amount sent to creation minus the fee");
+		[initiatorCurrentDeposit, counterpartCurrentDeposit] = await deposit.methods.viewCurrentDeposit().call();
+		assert.equal(initiatorCurrentDeposit, initialValue, "viewCurrentDeposit without parameters should return the initiator current deposited also");
+		assert.equal(counterpartCurrentDeposit, 0, "counterpart not set, current deposit should be 0");
+	});
   //it('allows people to contribute money and marks them as approvers', async () => {
     //await campaign.methods.contribute().send({
       //value: '200',
